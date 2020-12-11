41ec237bbda9fdac4829e564448e1727f9a6e5be
Dave Marcantonio <david.marcantonio@echosec.net>
2020-12-10 20:39:17 -0800

    Fixed the slicing, and getting the notifications number BEFORE we click the notification button.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// Get the number of new videos via the notifications bell # 
+const newVidCount = +document.querySelector("#notification-count").innerText;
+
 // Click the notifications button
 document.querySelector(".style-scope ytd-notification-topbar-button-renderer").click();
+
 // Give the api time to return the notifications list.
 setTimeout(() => {   
     const vidLinks = document.querySelectorAll('#items > ytd-notification-renderer > a[href]');
@@ -9,9 +13,8 @@ setTimeout(() => {
         ids.push(link.href.split('=')[1]);
     });
 
-    // Get the number of new notifications and make the playlist of that length.
-    const newVids = +document.querySelector("#notification-count").innerText;
-    const link = 'http://www.youtube.com/watch_videos?video_ids=' + ids.slice(0, newVids || ids.length).toString();
+    // Generate a playlist with the length of the new notifications count.
+    const link = 'http://www.youtube.com/watch_videos?video_ids=' + ids.slice(0, newVidCount || ids.length);
     // Redirect to playlist!
     window.location.replace(link);
 }, 2500);
